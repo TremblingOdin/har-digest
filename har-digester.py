628bf3226554d8f85ee01f164eecf29d0b0b1f4d
TremblingOdin <pigmekevin@gmail.com>
2020-12-01 00:27:15 -0700

    fleshed out the diff functionality

--- har-digester.py
+++ har-digester.py
@@ -1,13 +1,35 @@
 #!/usr/bin/python3
 
-import argparse,sys
+import argparse,json,sys
 
 def diff(flist, outfile):
     print('Creating the diff file in: ', outfile)
+    if len(flist) < 2:
+        print("Pass at least 2 files to be compared")
+    
     writer = open(outfile, 'w')
     try:
+        nJson = None
         for f in flist:
-            print(f)
+            print("Reading file: ", f)
+            reader = open(f, 'r')
+            data = json.load(reader)
+            if nJson is None:
+                nJson = data
+            else:
+                count = 0
+                for entry in nJson['log']['entries']:
+                    print("Comparing entry: ", count)
+                    count += 1
+                    for ent in data['log']['entries']:
+                        if ent['request'] == entry['request']:
+                           nJson['log']['entries'].remove(entry)
+                        else:
+                            nJson['log']['entries'].append(ent)
+
+
+            reader.close()
+        json.dump(nJson, writer)
     finally:
         writer.close()
 
